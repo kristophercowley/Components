08ffe65f1578ab88021fbe6533b2845f1fc3faad
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-05-04 16:56:47 -0600

    myComponent with arguements

---
+++ api.js
@@ -0,0 +1,135 @@
+angular.module('my-app',[])
+.controller('PlayerController', function($scope) {
+    var vm = this;
+    vm.proRoster = [];
+    vm.display = [];
+    vm.myTeam = [];
+  
+    //Test obj
+    vm.playerObj = {
+        fullname: "A",
+        photo: "www.boisecodeworks.com/assets/unify/img/bg/32.jpg",
+    }
+    // vm.myTeam.$add(vm.playerObj)
+    //Declare arrays and inserts test teams and positions info
+    $scope.teams = ['SF', "KC", "IND", "CHI", "PHI", "ARI", "DET", "GB", "DEN"];
+    $scope.positions = ['QB', 'FB', 'RB'];
+
+    // Create Custom Player
+    function CreatePlayer() {
+        this.fullname = $scope.playerName
+        this.position = $scope.playerPosition
+        this.id = $scope.playerNumber
+        this.pro_status = "Pending";
+        this.photo = "http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/";
+        this.pro_team = "Prospect";
+    }
+
+    // Adds Custom Player To My Team ????
+    $scope.addCustomPlayer = function() {
+        var tempPlayer = new CreatePlayer();
+        vm.myTeam.$add(tempPlayer);
+
+    }
+
+    // Adds player to my team
+    vm.addToMyTeam = function(index) {
+        // vm.myTeam.push(vm.display[index]);
+        vm.proRoster.splice(index, 1);
+        vm.display.splice(index, 1);
+        console.log(vm.myTeam);
+        //working on firebase
+        vm.myTeam.$add(vm.display[index])
+    }
+
+    //Removes player from vm.display/screen
+    vm.remove = function(index) {
+        console.log(index)
+        // vm.proRoster.splice(index, 1);
+        vm.proRoster.push(index);
+        // vm.myTeam.splice(index, 1);
+        vm.myTeam.$remove(index)
+    }
+
+    //Filters players by a chosen team
+    vm.getTeam = function(team) {
+        console.log("is getTeam working?")
+        vm.display = [];
+        for (var i = 0; i < vm.proRoster.length; i++) {
+            if (team === vm.proRoster[i].pro_team) {
+                vm.display.push(vm.proRoster[i]);
+            }
+        } console.log(vm.display)
+    }
+
+    //Filters players by a chosen position
+    vm.getPos = function(pos) {
+        console.log("is getPos working?")
+        vm.display = [];
+        for (var i = 0; i < vm.proRoster.length; i++) {
+            if (pos === vm.proRoster[i].position) {
+                vm.display.push(vm.proRoster[i]);
+            }
+        } console.log(vm.display)
+    }
+
+    //Finds all team tags and pushes to array
+    vm.popTeams = function(array) {
+        for (var i = 0; i < array.length; i++) {
+            if ($scope.teams.indexOf(array[i].pro_team) == -1)
+                $scope.teams.push(array[i].pro_team);
+        }
+    }
+    //vm.popTeams(vm.proRoster);
+
+    //Finds all position tags and pushes to array
+    vm.popPos = function(array) {
+        for (var i = 0; i < array.length; i++) {
+            if ($scope.positions.indexOf(array[i].position) == -1)
+                $scope.positions.push(array[i].position);
+        }
+    }
+
+    //Pulls players from api and pushes them to proRoster		
+    vm.drawPlayers = function(player) {
+        for (var i = 0; i < 1000; i++) {
+            var temp = {}
+            if (player[i].pro_status === "A" && player[i].photo !== "https://auth.cbssports.com/images/players/unknown-player-170x170.png") {
+                temp.fullname = player[i].fullname,
+                    temp.photo = player[i].photo,
+                    temp.pro_status = player[i].pro_status,
+                    temp.pro_team = player[i].pro_team,
+                    temp.id = player[i].id,
+                    temp.position = player[i].position
+                    vm.proRoster.push(temp),
+                    // sends angular fire a copy
+                    // vm.backup.$add(temp),
+                    // end copy
+                    vm.popTeams(vm.proRoster),
+                    vm.popPos(vm.proRoster)
+            }
+        }
+        console.log(vm.proRoster)
+    }
+
+
+    vm.requestor = function() {
+        $scope.data = "Loading Data..... ";
+        console.log("request sent");
+        vm.url = "http://bcw-getter.herokuapp.com/?url=";
+        vm.url2 = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+        vm.apiUrl = vm.url + encodeURIComponent(vm.url2);
+        $.get(vm.apiUrl).success(function(res) {
+            vm.resToObj = JSON.parse(res);
+            vm.liveRoster = vm.resToObj.body.players;
+            console.log(vm.liveRoster);
+            vm.drawPlayers(vm.liveRoster);
+            $scope.data = "Data Loaded";
+            $scope.$apply();
+            
+
+        })
+    }
+    vm.requestor();
+
+})
---
+++ app.js
@@ -0,0 +1,43 @@
+(function () {
+    function MeController() {
+        var mc = this;
+        mc.test = { name: "Testing" };
+        mc.$onInit = function () {
+        }
+        mc.getData = function () {
+            $('#butt').text('FGHJKL');
+            setTimeout(function () {
+                $('#butt').text('Get Data');
+
+            }, 1000)
+        }
+    }
+
+    angular.module('my-app', [])
+        .controller('MainController', function () {
+            var mc = this;
+            mc.me = { name: "Kris" }
+            mc.you = { name: "Yous" }
+            mc.num = { name: 999 }
+            mc.h1 = { title: "Component 1" }
+
+        })
+        .component('myComponent', {
+            templateUrl: 'myComponent.html',
+            controller: MeController,
+            bindings: {
+                me: '=',
+                you: '<',
+                num: '=',
+                test: '&',
+                h1: '='
+            },
+            controllerAs: 'vm'
+
+        })
+        .component('pending', {
+            controller: function () {
+
+            }
+        })
+})();
